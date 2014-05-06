213e917c5df72689c72aa19e63b2ff786e5d422d
rstuemer <robert.stuemer@googlemail.com>
2014-05-06 09:38:50 +0200

    Initial Commit add GuiceJunitRunner and GuiceAnnotation for JUnitTests

---
+++ src/main/java/com/qudosoft/guicejunitrunner/annotation/Guice.java
@@ -0,0 +1,20 @@
+package com.qudosoft.guicejunitrunner.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import com.google.inject.Module;
+
+/**
+ * This annotation specifies what Guice modules should be used.
+ * */
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target(TYPE)
+public @interface Guice {
+	/**
+	 * @return the list of modules
+	 */
+	Class<? extends Module>[] modules () default {};
+}
---
+++ src/main/java/com/qudosoft/guicejunitrunner/annotation/GuiceJunitRunner.java
@@ -0,0 +1,100 @@
+/**
+ * 
+ */
+package com.qudosoft.guicejunitrunner.annotation;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.InitializationError;
+
+import com.google.inject.Injector;
+import com.google.inject.Module;
+
+/**
+ * 
+ *
+ */
+public class GuiceJunitRunner extends BlockJUnit4ClassRunner {
+	
+	private static final Logger LOGGER = Logger.getLogger(GuiceJunitRunner.class.getName());
+	private final Injector injector;
+	
+	/**
+	 * @param klass
+	 * @throws InitializationError
+	 */
+	public GuiceJunitRunner(Class<?> clazz) throws InitializationError {
+		super(clazz);
+		Annotation annotation = findAnnotationSuperClasses(Guice.class, clazz);
+		Guice guice = (Guice) annotation;
+		Module[] modules = getModules(guice, clazz);
+		injector = createInjectorFor(modules);
+	}
+	
+	
+	@Override
+	public Object createTest() throws Exception {
+		Object obj = super.createTest();
+		injector.injectMembers(obj);
+		return obj;
+	}
+	
+	
+	/**
+	 * Returns the Modules defined on TestClass with {@link Guice} Annotation
+	 * 
+	 * @param guice Annotation
+	 * @param testClass
+	 * @return
+	 */
+	private Module[] getModules(Guice guice, Class<?> testClass) {
+		List<Module> result = new ArrayList<Module>();
+		for (Class<? extends Module> moduleClass : guice.modules()) {
+			Module instance;
+			try {
+				instance = moduleClass.newInstance();
+				LOGGER.info("Create Instance for Module:" + instance.getClass().getName());
+				result.add(instance);
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		return result.toArray(new Module[result.size()]);
+	}
+	
+	/**
+	 * 
+	 * Create an Injctor with the given modules
+	 * 
+	 * @param modules
+	 * @return
+	 */
+	private Injector createInjectorFor(Module[] modules) {
+		return com.google.inject.Guice.createInjector(modules);
+		
+	}
+	
+	
+	// TODO: move to Utils
+	private static Annotation findAnnotationSuperClasses(Class annotationClass, Class c) {
+		while (c != null) {
+			Annotation result = c.getAnnotation(annotationClass);
+			if (result != null) {
+				return result;
+			}
+			else {
+				c = c.getSuperclass();
+			}
+		}
+		return null;
+	}
+	
+	
+}
